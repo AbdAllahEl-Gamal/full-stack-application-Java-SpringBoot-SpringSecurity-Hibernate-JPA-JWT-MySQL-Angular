ff2d3f58f981b0a9d878fcfb6375588df9d53552
AbdAllah El Gamal <abdallah.algamal93@gmail.com>
2020-04-13 20:07:00 +0530

    some changes

--- src/main/java/com/registration/controller/AuthController.java
+++ src/main/java/com/registration/controller/AuthController.java
@@ -19,6 +19,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ import java.net.URI;
 import java.util.Collections;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/auth")
 public class AuthController {
 
